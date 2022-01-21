49b7446b850e230d55b9005858fa88d5d7496b52
Hiking <tom_xj_cheng@hotmail.com>
2022-01-21 18:04:38 +0800

    build: Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.serialization") version "1.5.31"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
 }
 group = "com.hiking"
 version = "0.0.1"
@@ -12,12 +12,12 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.3.2")
     testImplementation(kotlin("test-junit"))
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 application {
-    mainClassName = "MainKt"
+    mainClass.set("MainKt")
 }
